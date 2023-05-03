a9a38551efa9118d22a36019a53eccc35345d586
code-death <poorvanshkavta938@gmail.com>
2023-05-04 05:09:51 +0530

    Layot Sorted

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,36 @@
-
+import { BrowserRouter } from "react-router-dom";
+import {
+  About,
+  Contact,
+  Experience,
+  Feedbacks,
+  Hero,
+  Navbar,
+  Tech,
+  Works,
+  StarsCanvas,
+} from "./components";
 
 function App() {
-
   return (
-    <div>
-      3D portfolio
-    </div>
-  )
+    <BrowserRouter>
+      <div className="relative z-0 bg-primary">
+        <div className="bg-hero-pettern bg-cover bg-null-repeat bg-center">
+          <Navbar />
+          <Hero />
+        </div>
+          <About />
+          <Experience />
+          <Tech />
+          <Works />
+          <Feedbacks />
+        <div className="relative z-0">
+           <Contact />
+           <StarsCanvas />
+        </div>
+      </div>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
